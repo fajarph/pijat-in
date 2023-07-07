b18c32870bc1dc1e3ceed8fbac01c51eea9885aa
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-07-07 20:15:52 +0700

    Update env.js

--- build/env.js
+++ build/env.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const Env_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Env"));
 exports.default = Env_1.default.rules({
-    HOST: Env_1.default.schema.string({ format: 'host' }).procces.env.HOST,
+    HOST: Env_1.default.schema.string({ format: 'host' }),
     PORT: Env_1.default.schema.number(),
     APP_KEY: Env_1.default.schema.string(),
     APP_NAME: Env_1.default.schema.string(),
