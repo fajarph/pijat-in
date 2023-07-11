09dcf457000268fdadc6d896bbe54ba9d1a711c0
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-07-11 13:40:24 +0700

    Update 1688618313861_orders.ts

--- database/migrations/1688618313861_orders.ts
+++ database/migrations/1688618313861_orders.ts
@@ -1,11 +1,23 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
+function generateRandomValue(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  let result = ''
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length)
+    result += characters.charAt(randomIndex)
+  }
+
+  return result
+}
+
 export default class extends BaseSchema {
   protected tableName = 'orders'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
+      table.string('id').primary().defaultTo(generateRandomValue(5))
       table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
       table.string('nama_lengkap', 255).notNullable()
       table.string('gender', 100).notNullable()
