96deb0ed6309c98d59ec94ad1a45e2006be351a4
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-07-22 10:58:26 +0700

    Update sendEmail.ts

--- app/Util/sendEmail.ts
+++ app/Util/sendEmail.ts
@@ -1,7 +1,10 @@
 import nodemailer from "nodemailer"
 
-const {SMTP_EMAIL, SMTP_PASS} = process.env
+const {SMTP_HOST, SMTP_PORT, SMTP_EMAIL, SMTP_PASS} = process.env
 let transporter = nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    secure: true,
     auth: {
         user: SMTP_EMAIL,
         pass: SMTP_PASS,
