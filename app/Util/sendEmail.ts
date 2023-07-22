f9b529f52fbead497a3135d44bebe0f13839a2fe
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-07-22 11:35:23 +0700

    Update sendEmail.ts

--- app/Util/sendEmail.ts
+++ app/Util/sendEmail.ts
@@ -4,7 +4,7 @@ const {SMTP_EMAIL, SMTP_PASS} = process.env
 let transporter = nodemailer.createTransport({
     service: "outlook",
     port: 465,
-    secure: true,
+    secure: false,
     auth: {
         user: SMTP_EMAIL,
         pass: SMTP_PASS,
