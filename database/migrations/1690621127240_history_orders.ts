964252d393acf6859853f79b5f003fae2f02a9fa
Fajar Putra <106464666+fajarph@users.noreply.github.com>
2023-08-11 14:17:19 +0700

    Update 1690621127240_history_orders.ts

--- database/migrations/1690621127240_history_orders.ts
+++ database/migrations/1690621127240_history_orders.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('status_sebelumnya')
+      table.string('status_sebelumnya').defaultTo("Dijadwalkan")
       table.integer('order_id').unsigned().references('id').inTable('orders').onDelete('CASCADE')
       table.datetime('created_history')
 
